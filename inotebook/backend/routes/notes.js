491c207cf36bd01944070b8ca380d656f217615f
Arbaz Khan <arbazkhaann256@gmail.com>
2024-03-22 22:21:25 +0500

    Update Note Router added

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -29,11 +29,11 @@ router.post("/addnote", fetchuser, [
         return res.status(400).json({ errors: errors.array() });
     }
 
-    const {title ,description, tag} = req.body;
+    const { title, description, tag } = req.body;
 
     try {
         const note = new notesModel({
-            title ,description, tag, user:req.user.id
+            title, description, tag, user: req.user.id
         })
         const savedNotes = await note.save();
 
@@ -43,6 +43,33 @@ router.post("/addnote", fetchuser, [
         console.error(error.message);
         res.status(500).send("Internal Server Error");
     }
+})
+
+//ROUTE 3: Update Note using PUT "/api/notes/addnote". Login Required.
+router.put("/updatenote/:id", fetchuser, async (req, res) => {
+
+    try {
+        const { title, description, tag } = req.body;
+
+        let newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        let note = await notesModel.findById(req.params.id);
+        if (!note) { return res.status(404).send("Note Not Found") };
+
+        if (note.user.toString() !== req.user.id) { return res.status(401).send("Not Available") };
+
+        note = await notesModel.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json(note);
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+
 })
 
 module.exports = router; 
